47b9ae51c262b0c17aca311ccbb8366655f35f39
pravosleva <selection4test@gmail.com>
2017-12-09 23:42:12 +0300

    Updates
    - callbacks added

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 function onCheckBoxChange (checkBox) {
   if (checkBox.checked) {
-    var flag = true;
-    var handler = function(event) {
-      if (!checkBox.checked) {
-        flag = false;
-        checkBox.removeEventListener('change', handler);
-        document.getElementById('resultTable').innerHTML = '<span>Polling switched off.</span>';
-        document.getElementById('resultTable').style.background = 'yellow';
-        document.getElementById('resultTable').style.color = 'black';
-      }
-    };
-    checkBox.addEventListener ("change", handler);
-    startPolling ({
+    var switched_ON = true;
+    var handlerForCheckBox = (event) => {
+        if (!checkBox.checked) {
+          switched_ON = false;
+          checkBox.removeEventListener('change', handlerForCheckBox);
+          document.getElementById('resultTable').innerHTML = '<span>Polling switched off.</span>';
+          document.getElementById('resultTable').style.background = 'yellow';
+          document.getElementById('resultTable').style.color = 'black';
+        } else { /* nothing */ }
+      },
+      callbackAsResolve = (jetsArr) => {
+        console.table(jetsArr);
+        document.getElementById('resultTable').innerHTML = _getTableHTML(jetsArr);
+        document.getElementById('resultTable').style.background = '#374c6b';
+        document.getElementById('resultTable').style.color = 'white';
+        document.getElementById('reportTime').innerHTML = new Date();
+      },
+      callbackAsReject = (err) => {
+        console.error(err);
+        document.getElementById('resultTable').innerHTML = '<span>Error: ' + err + '</span>';
+        document.getElementById('resultTable').style.background = 'red';
+        document.getElementById('resultTable').style.color = 'white';
+      };
+    checkBox.addEventListener ("change", handlerForCheckBox);
+    startPollingByConditions ({
       url: 'http://selection4test.ru:1111/jetsArray',
-      toBeOrNotToBe: () => { return (flag); },
-      interval: 3000
+      toBeOrNotToBe: () => switched_ON,
+      interval: 3000,
+      callbackAsResolve,
+      callbackAsReject
     });
   }
 }
@@ -31,34 +46,30 @@ function myAsyncRequest (url) {
 
 function myTimeoutPromise (ms) {
   return new Promise((resolve, reject) => {
-    setTimeout(function(){resolve();}, ms);
+    setTimeout(() => resolve(), ms);
   });
 }
 
-function startPolling (arg) {
-  let { url, toBeOrNotToBe, interval } = arg;
-  console.log ("startPolling ()", url, toBeOrNotToBe(), interval);
+function startPollingByConditions (arg) {
+  let { url, toBeOrNotToBe, interval, callbackAsResolve, callbackAsReject } = arg;
+  console.log ("startPollingByConditions ()", url, toBeOrNotToBe(), interval);
   if (toBeOrNotToBe()) {
     myAsyncRequest (url)
       .then (function (result){
-        console.table(result);
-        document.getElementById('resultTable').innerHTML = _getTableHTML(result);
-        document.getElementById('resultTable').style.background = '#374c6b';
-        document.getElementById('resultTable').style.color = 'white';
-        document.getElementById('reportTime').innerHTML = new Date();
+        console.log (`startPollingByConditions () is done.`);
+        callbackAsResolve(result);
         return (myTimeoutPromise (interval));
       })
       .then (function (){
-        startPolling ({ url, toBeOrNotToBe, interval });
+        console.log (`startPollingByConditions () was called again...`);
+        startPollingByConditions ({ url, toBeOrNotToBe, interval, callbackAsResolve, callbackAsReject });
       })
       .catch (function (err) {
-        console.log ("An error occured.");
-        document.getElementById('resultTable').innerHTML = '<span>Error: ' + err + '</span>';
-        document.getElementById('resultTable').style.background = 'red';
-        document.getElementById('resultTable').style.color = 'white';
-        startPolling ({ url, toBeOrNotToBe, interval });
+        console.log (`startPollingByConditions () is failed: ${err}`);
+        callbackAsReject(err);
+        startPollingByConditions ({ url, toBeOrNotToBe, interval, callbackAsResolve, callbackAsReject });
       });
-  }
+  } else { }
 }
 
 function myTimeoutPromise (ms) {
